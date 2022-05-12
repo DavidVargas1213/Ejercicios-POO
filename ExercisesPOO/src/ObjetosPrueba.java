dbfc5e38270dccaab39df37ad1b48c081a06dc1f
DavidVargas1213 <davidvargas.dv6@gmail.com>
2022-05-11 19:49:01 -0500

    Update ObjetosPrueba.java

--- ExercisesPOO/src/ObjetosPrueba.java
+++ ExercisesPOO/src/ObjetosPrueba.java
@@ -3,11 +3,8 @@ public class ObjetosPrueba {
 
 	public static void main(String[] args) {
 		Rectangle Rectangle1  = new Rectangle();
-
 		System.out.println(Rectangle1.getArea()+"" + "" +Rectangle1.getPerimeter());
-		
 		Rectangle Rectangle2 = new Rectangle(5 , 6); 
-		
 		System.out.println(Rectangle2.getArea()+"aa" + "" +Rectangle1.getPerimeter());
 	}
 
